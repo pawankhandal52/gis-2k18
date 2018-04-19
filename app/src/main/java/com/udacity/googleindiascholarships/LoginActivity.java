2245701e64dcd5496772144d47882edbabc0ba33
Pawan <pksharama@stemdot.com>
2018-04-19 18:51:47 +0530

    Bug fix back button

--- app/src/main/java/com/udacity/googleindiascholarships/LoginActivity.java
+++ app/src/main/java/com/udacity/googleindiascholarships/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.udacity.googleindiascholarships;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
     
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    
+    public void login(View view) {
+        Intent intent  =new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+    
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+        
     }
 }
